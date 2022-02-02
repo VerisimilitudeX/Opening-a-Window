be91bc31b102eab133f7109178e4cac953d3c718
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-01 19:18:21 -0800

    Add files via upload

---
+++ Opening_a_Window_Practice_1.py
@@ -0,0 +1,20 @@
+"""
+LESSON: 3.1 - Lines
+TECHNIQUE 1: Opening a Window
+PRACTICE 1
+"""
+
+# Import and initialize pygame
+import pygame
+pygame.init()
+
+# Open the window
+w = pygame.display.set_mode([400,300])
+
+
+# Wait 5 seconds                                   
+pygame.time.wait(5000)
+
+
+# Turn in your Coding Exercise.
+
