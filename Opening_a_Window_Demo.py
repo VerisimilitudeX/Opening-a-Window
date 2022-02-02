33330ed90ffcac5657821695da4e334c177aca1d
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-01 19:13:16 -0800

    Add files via upload

---
+++ Opening_a_Window_Demo.py
@@ -0,0 +1,17 @@
+"""
+LESSON: 3.1 - Lines
+TECHNIQUE 1: Opening a Window
+DEMO
+"""
+import random
+import pygame
+pygame.init()
+w = pygame.display.set_mode([400,300])
+w.fill((255,255,255))
+
+rand1 = random.randint(0,300)
+rand2 = random.randint(0,300)
+
+pygame.draw.line(w, (0,0,0), (50, rand1), (350, rand2), 10)
+
+pygame.display.flip()
