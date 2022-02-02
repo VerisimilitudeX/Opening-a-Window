ac2aec48195f6c17430bf6e9944448cde2186e3c
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-01 19:21:07 -0800

    Add files via upload

---
+++ Opening_a_Window_Practice_2.py
@@ -0,0 +1,22 @@
+"""
+LESSON: 3.1 - Lines
+TECHNIQUE 1: Opening a Window
+PRACTICE 2
+"""
+# Import and initialize pygame
+import pygame
+pygame.init()
+
+
+# Get the width and height from the user
+width = int(input("Please input the width "))
+height = int(input("Please input the height "))
+
+# Open the window
+w = pygame.display.set_mode([width,height])
+
+# Wait 5 seconds
+pygame.time.wait(5000)
+
+
+# Turn in your Coding Exercise.
