ae937c429e5f81198a22a1ece6dcc0f8bc73fe2c
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-03 16:05:37 -0800

    Update Opening_a_Window_Practice_1.py

--- Opening_a_Window_Practice_1.py
+++ Opening_a_Window_Practice_1.py
@@ -1,9 +1,3 @@
-"""
-LESSON: 3.1 - Lines
-TECHNIQUE 1: Opening a Window
-PRACTICE 1
-"""
-
 # Import and initialize pygame
 import pygame
 pygame.init()
@@ -14,7 +8,3 @@ w = pygame.display.set_mode([400,300])
 
 # Wait 5 seconds                                   
 pygame.time.wait(5000)
-
-
-# Turn in your Coding Exercise.
-
