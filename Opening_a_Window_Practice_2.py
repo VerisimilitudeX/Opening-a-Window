e435baacb2917cfab4e57899471fe2344aad4e00
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 16:58:08 -0800

    Update Opening_a_Window_Practice_2.py

--- Opening_a_Window_Practice_2.py
+++ Opening_a_Window_Practice_2.py
@@ -1,14 +1,9 @@
-# Import and initialize pygame
 import pygame
 pygame.init()
 
-
-# Get the width and height from the user
 width = int(input("Please input the width "))
 height = int(input("Please input the height "))
 
-# Open the window
 w = pygame.display.set_mode([width,height])
 
-# Wait 5 seconds
 pygame.time.wait(5000)
