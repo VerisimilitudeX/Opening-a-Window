df2403473186f46ff0afa6b7c9ada8c41350857d
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 16:57:47 -0800

    Update Opening_a_Window_Practice_1.py

--- Opening_a_Window_Practice_1.py
+++ Opening_a_Window_Practice_1.py
@@ -1,10 +1,6 @@
-# Import and initialize pygame
 import pygame
 pygame.init()
 
-# Open the window
 w = pygame.display.set_mode([400,300])
-
-
-# Wait 5 seconds                                   
+                                   
 pygame.time.wait(5000)
